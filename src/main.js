f59a626db2ab6e1a9aeb1f2f78e1cca5d16fa2da
James Fidler <terrarum@gmail.com>
2016-01-25 23:52:18 +0000

    Added Firebase with auth.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 var Firebase = require('firebase');
 var Vue = require('vue');
 
+
+var iomante = new Firebase('https://iomante.firebaseio.com/');
+
+
+
 var startTemplate = require('html!./templates/start.html');
 
 new Vue({
   el: '.app-container',
   template: startTemplate,
   data: {
-    message: "HELLO"
+    email: '',
+    password: ''
+  },
+  methods: {
+    login: function() {
+      iomante.authWithPassword({
+        email    : this.email,
+        password : this.password
+      }, function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+        }
+      });
+
+    }
   }
 });
